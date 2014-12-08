39b9e5e3fee55872d9dc8803c5f401ecb0cdb8b0
AhmedAtef07 <ahmedatef07@hotmail.com>
2014-12-08 20:28:05 +0200

    Creating the project.

---
+++ src/elpaint/ElPaint.java
@@ -0,0 +1,16 @@
+package elpaint;
+
+/**
+ *
+ * @author Ahmed Atef
+ */
+public class ElPaint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
