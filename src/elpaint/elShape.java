88872d6a2cd9abf2a6eb40e26e2d5603ea508551
Ahmed Wael Said <ahmedws93@hotmail.com>
2014-12-09 02:13:22 +0200

    Class elShape

--- src/elpaint/elShape.java
+++ src/elpaint/elShape.java
@@ -1,9 +1,35 @@
 package elpaint;
 
+import java.awt.Shape;
+
 /**
  *
- * @author Ahmed Atef
+ * @author HackerGhost
  */
-public class elShape {
+public abstract class elShape implements Shape {
+ 
+ private boolean IsSelected;
 
+     
+    public elShape() {
+
+        this.IsSelected = false;
+    }
+
+    /**
+     * @return the IsSelected
+     */
+    public boolean isIsSelected() {
+        return IsSelected;
+    }
+
+    /**
+     * @param IsSelected the IsSelected to set
+     */
+    public void setIsSelected(boolean IsSelected) {
+        this.IsSelected = IsSelected;
+    }
+    
+    
+ 
 }
