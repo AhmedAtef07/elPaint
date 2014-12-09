daee2029952235708f0dbb3bff8b742326ec25e5
Ahmed Wael Said <ahmedws93@hotmail.com>
2014-12-09 02:30:28 +0200

    elShape update abstarct method.

--- src/elpaint/elShape.java
+++ src/elpaint/elShape.java
@@ -1,27 +1,49 @@
 package elpaint;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Shape;
+import java.awt.Stroke;
 
 /**
  *
  * @author HackerGhost
  */
-public abstract class elShape implements Shape {
+public abstract class elShape  {
  
  private boolean IsSelected;
 
-     
+     Stroke LineType;
+     Color  FillColor;
+     Color  BorderColor;
     public elShape() {
 
         this.IsSelected = false;
+        this.LineType = new BasicStroke(2);
+        this.FillColor = Color.WHITE;
+        this.BorderColor = Color.BLACK;
     }
+    public elShape(Color FillColor,Color BorderColor,Stroke LineType){
+        this.FillColor= FillColor;
+        this.BorderColor= BorderColor;
+        this.LineType = LineType ;
+        this.IsSelected= false;
+    }
+    
+    /**
+     * @param mask a mask within the type of object to be returned
+     * @
+     * @return object which has the implementation of shape 
+     */
+    public abstract Shape getFloat();
 
     /**
      * @return the IsSelected
      */
-    public boolean isIsSelected() {
+    public boolean getIsSelected() {
         return IsSelected;
     }
+    
 
     /**
      * @param IsSelected the IsSelected to set
