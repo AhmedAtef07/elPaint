98ae16ff045ac1b45de68f926ce480a0f0557bbb
Hassan Rezk <hassan.rms93@gmail.com>
2014-12-13 16:50:16 +0200

    Adding Properties enum

--- src/elpaint/elComponent.java
+++ src/elpaint/elComponent.java
@@ -2,8 +2,23 @@ package elpaint;
 
 /**
  *
- * @author Ahmed Atef
+ * @author Hassan Rezk
  */
 public class elComponent {
-
+    
+    public enum PropertyType {
+        TEXT,
+        CHECKBOX,
+        SLIDER,
+        LABEL,
+    }
+    
+    public enum Properties {
+        X,
+        Y,
+        WIDTH,
+        HEIGHT,
+        COLOR,
+        STROKE,
+    }
 }
