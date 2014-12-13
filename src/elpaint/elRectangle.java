7ac98703ecb95a3e651d15fdf22f40309a935bdf
AhmedAtef07 <ahmedatef07@hotmail.com>
2014-12-13 17:03:44 +0200

    Updating 'elRectangle' to receive Point, width, height.

--- src/elpaint/elRectangle.java
+++ src/elpaint/elRectangle.java
@@ -13,43 +13,23 @@ import java.awt.geom.Rectangle2D;
  */
 public class elRectangle extends elPolygon {
     
-    //Point[] x ;
-    public elRectangle(Color FillColor,Color BorderColor,Stroke LineType
-            ,Point p1 , Point p2){
-        
-        
-        super(FillColor,BorderColor,LineType,p1,p2);
+    public elRectangle (int x, int y, int width, int height) {
+        super(new Point(x, y), new Point(x + width, y + height));
+    }    
+    
+    public elRectangle (int x, int y, int width, int height, 
+                Color FillColor, Color BorderColor ,Stroke LineType) {
+        super(FillColor, BorderColor, LineType, 
+                new Point(x, y), new Point(x + width, y + height));
     }
-    
-    public elRectangle(Point p1 , Point p2){
-        super(p1,p2);
-    }
-    
-    
+      
     @Override
     public Shape getShape() {
             int x = Math.min(getPoint(0).x, getPoint(1).x);
-            int y = Math.min(getPoint(0).y,getPoint(0).y);
-            int width = Math.abs(getPoint(1).x-getPoint(0).x);
-            int height = Math.abs(getPoint(1).y-getPoint(0).y);
-            return new Rectangle2D.Float(x,y,width,height);
-    }
-
-   
-
-    @Override
-    public boolean hasStroke() {
-        return true ;
-    }
-
-    @Override
-    public boolean hasFillColor() {
-        return true ;
-    }
-
-    @Override
-    public boolean hasBorderColor() {
-        return true ;
+            int y = Math.min(getPoint(0).y, getPoint(0).y);
+            int width = Math.abs(getPoint(1).x - getPoint(0).x);
+            int height = Math.abs(getPoint(1).y - getPoint(0).y);
+            return new Rectangle2D.Float(x, y, width, height);
     }
     
 }
