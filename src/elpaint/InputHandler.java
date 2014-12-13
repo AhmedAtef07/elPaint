71ad6f7a749e3dbb0c708583dfe1e0ae275f168e
AhmedAtef07 <ahmedatef07@hotmail.com>
2014-12-13 17:50:10 +0200

    Input handling using 'Triggable'.

--- src/elpaint/InputHandler.java
+++ src/elpaint/InputHandler.java
@@ -1,9 +1,70 @@
 package elpaint;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 /**
  *
  * @author Ahmed Atef
  */
-public class InputHandler {
+public class InputHandler implements MouseMotionListener, MouseListener,
+        KeyListener {
+    
+    Triggable triggable;
+    public InputHandler(Triggable triggable) {
+        this.triggable = triggable;
+    }
+    
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        triggable.mouseDragged(e);
+    }
 
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        triggable.mouseMoved(e);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        triggable.mouseClicked(e);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        triggable.mousePressed(e);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        triggable.mouseReleased(e);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        triggable.mouseEntered(e);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        triggable.mouseExited(e);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        triggable.keyTyped(e);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        triggable.keyPressed(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        triggable.keyReleased(e);
+    }
 }
--- src/elpaint/Stage.java
+++ src/elpaint/Stage.java
@@ -1,10 +1,13 @@
 package elpaint;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
 /**
  *
  * @author Ahmed Atef
  */
-public class Stage {
+public class Stage implements Triggable {
     
     private UserInterface ui;
     private Layer layer;
@@ -13,4 +16,53 @@ public class Stage {
         ui = new UserInterface(this);
         layer = ui.getLayer();
     }       
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) { 
+        
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {   
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {  
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+    }
+    
 }
