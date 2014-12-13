be3c6bea862215fa410dea3ec31a9e1c8571b16d
AhmedAtef07 <ahmedatef07@hotmail.com>
2014-12-13 20:56:12 +0200

    Draw rectangle on drag and release.

--- src/elpaint/Stage.java
+++ src/elpaint/Stage.java
@@ -1,7 +1,11 @@
 package elpaint;
 
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.Shape;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 /**
  *
@@ -12,33 +16,66 @@ public class Stage implements Triggable {
     private UserInterface ui;
     private Layer layer;
     
+    private int startX, startY;
+    private elShape holdedShape;
+    
     public Stage() {
         ui = new UserInterface(this);
         layer = ui.getLayer();
+        
+        resetDrawingFactors();
     }       
 
+    void resetDrawingFactors() {        
+        startX = -1;
+        startY = -1;
+    }
+         
+    private void drawHoldedShape(int pressedX, int pressedY, int width,
+            int height) {            
+        holdedShape = new elRectangle(pressedX, pressedY, width, height);         
+        holdedShape.setFillColor(Color.yellow);
+        holdedShape.setBorderColor(Color.red);
+        layer.setHoldedShape(holdedShape);
+        layer.repaint();
+    }
+    
     @Override
     public void mouseDragged(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) { 
+        Point point = e.getPoint();
+        int x = point.x - ui.getComponents()[0].getX();
+        int y = point.y - ui.getComponents()[0].getY();
+        if (startX == -1) {
+            startX = x;
+            startY = y;
+        } 
+        int minX = Math.min(x, startX);
+        int minY = Math.min(y,startY);
+        int width = Math.abs(minX - Math.max(startX, x));
+        int height = Math.abs(minY - Math.max(startY, y));
+        drawHoldedShape(minX, minY, width, height); 
         
     }
 
     @Override
-    public void mouseClicked(MouseEvent e) {   
-        
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        
     }
 
     @Override
-    public void mouseReleased(MouseEvent e) {  
-        
+    public void mouseReleased(MouseEvent e) {
+        resetDrawingFactors();
+        layer.addShape(holdedShape);
+        layer.setHoldedShape(null);
+        layer.repaint();
     }
 
     @Override
@@ -54,15 +91,13 @@ public class Stage implements Triggable {
     public void keyTyped(KeyEvent e) {
 
     }
-    
+
     @Override
     public void keyPressed(KeyEvent e) {
-        
+    
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        
     }
-    
 }
