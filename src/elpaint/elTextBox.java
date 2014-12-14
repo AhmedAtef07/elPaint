ac9a5374dc2205e329f96ba42896fdd94fa027c9
Hassan Rezk <hassan.rms93@gmail.com>
2014-12-14 16:49:28 +0200

    adding attributes in 'elTextBox' class

--- src/elpaint/elTextBox.java
+++ src/elpaint/elTextBox.java
@@ -1,9 +1,64 @@
 package elpaint;
 
+import java.awt.*;
+
 /**
  *
  * @author Hassan Rezk
  */
-public class elTextBox {
+public class elTextBox extends elComponent {
+    
+    private String text;
+    private Font font;
+    private Color color;
+    private int x, y; // lower left corner of the enclosing textbox
+    
+    public elTextBox(String text, Font font, Color color, int x, int y) {
+        this.text = text;
+        this.font = font;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Font getFont() {
+        return font;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setFont(Font font) {
+        this.font = font;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
     
 }
