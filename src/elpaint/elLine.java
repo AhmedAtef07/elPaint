f0abddfbcc3c011b9ac3926d51173b5a267216ad
Hassan Rezk <hassan.rms93@gmail.com>
2014-12-16 01:51:38 +0200

    drawing 'elLine'.

--- src/elpaint/elLine.java
+++ src/elpaint/elLine.java
@@ -10,82 +10,57 @@ import java.awt.geom.*;
 
 public class  ElLine extends ElShape {
 
-    private Point p1, p2;
-    private Color color;
-    
-    public Point getP1() {
-        return p1;
-    }
-
-    public Point getP2() {
-        return p2;
-    }
-
-    public void setP1(Point p1) {
-        this.p1 = p1;
-    }
-
-    public void setP2(Point p2) {
-        this.p2 = p2;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public double getAngle() {
-        return angle;
-    }
-
-    public ElLine(Point p1, Point p2) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.color = Color.BLACK;
-        linedraw = new Line2D.Float(p1, p2); 
-    }
-
-    public ElLine(Point p1, Point p2, Color color) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.color = color;
-        linedraw = new Line2D.Float(p1, p2); 
-    }
-    
-    public Shape getShape() {
-        return new Line2D.Float(p1, p2);
-    }
-    
-    private Line2D.Float linedraw;
-    private double angle;
-    
-    public void Rotate(double angle) {
-        this.angle = angle;
-        Rectangle rect = linedraw.getBounds();
-        AffineTransform transform = AffineTransform.getRotateInstance(angle, 
-                rect.getCenterX(), rect.getCenterY());
-        transform.createTransformedShape((Shape) this);
-    }
-    
-    public int getX() {
-        return linedraw.getBounds().x;
-    }
-
-    public int getY() {
-        return linedraw.getBounds().y;
-    }
-    
-    public void setX(int x) {
-        p1.x += x;
-        p2.x += x;
-    }
-
-    public void setY(int y) {
-        p1.y += y;
-        p2.y += y;
+    @Override
+    public ElShape getCopy() {
+        return new ElLine(p1,p2);
     }
 
     @Override
-    public ElShape getCopy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Shape getShape() {
+        int[] tx = new int[2];
+        int[] ty = new int[2];
+        tx[0] = p1.x;
+        tx[1] = p2.x;
+        ty[0] = p1.y;
+        ty[1] = p2.y;
+        return new Polygon(tx,ty,2);
+    }
+    
+    private Point p1,p2;
+    private int width,height;
+    
+    public ElLine(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.x = p1.x;
+        this.y = p1.y;
+        this.width = Math.abs(p2.x - p1.x);
+        this.height = Math.abs(p2.y - p1.y);
+    }
+    
+    @Override
+    public void move(int xDisplacment, int yDisplacment) {
+        setX(getClonedX()-xDisplacment);
+        setY(getClonedY()-yDisplacment);
+    }
+    
+    @Override
+    public void setX(int x) {
+        this.x = x;
+        Point p = new Point(x, y);
+        p1 = new Point(p);
+        p2 = new Point(p1.x, p1.y + height);
+        this.width = Math.abs(p2.x - p1.x);
+        this.height = Math.abs(p2.y - p1.y);
+    }
+    
+    @Override
+    public void setY(int y) {
+        this.y = y;
+        Point p = new Point(x,y);
+        p1 = new Point(p);
+        p2 = new Point(p1.x, p1.y + height);
+        this.width = Math.abs(p2.x - p1.x);
+        this.height = Math.abs(p2.y - p1.y);
     }
 }
