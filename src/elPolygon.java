19c0931b4d09b3a0af5bef40a03840ce82d82813
Hassan Rezk <hassan.rms93@gmail.com>
2014-12-16 18:05:07 +0200

    editing 'elPolygon'

--- src/elPolygon.java
+++ src/elPolygon.java
@@ -1,5 +1,3 @@
-
-
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 /**
@@ -18,12 +16,13 @@ public class elPolygon extends ElShape {
     public elPolygon(Point p1 , Point p2 ,int n , double angle){
         super();
         ptx = new int[n];
-        pty = new int[n];
+        pty = new int[n];   
         this.CenterX = p1.x;
         this.CenterY = p1.y;
         this.n = n;
         this.angel = angel ;
-        this.radius = (int) Math.sqrt(Math.pow(p2.x - p1.x,2) + Math.pow(p2.y-p1.y,2));
+        this.radius = (int) Math.sqrt(Math.pow(p2.x - p1.x,2) + 
+                Math.pow(p2.y-p1.y,2));
         this.width = 2*radius;
         this.height= 2*radius;
         System.out.println(radius);
@@ -73,10 +72,27 @@ public class elPolygon extends ElShape {
     }
     private void makePoints(){ 
         for (int i = 0; i < getN(); i++) {
-            ptx[i] = (int)(this.getCenterX() + getRadius() * Math.cos(2 * Math.PI * i / getN()));
-            pty[i] = (int)(this.getCenterY() + getRadius() * Math.sin(2 * Math.PI * i / getN()));
+            ptx[i] = (int)(this.getCenterX() + getRadius() * 
+                    Math.cos(2 * Math.PI * i / getN()));
+            pty[i] = (int)(this.getCenterY() + getRadius() * 
+                    Math.sin(2 * Math.PI * i / getN()));
         }
     }
+    
+    public int[] getXpts() {
+        int[] pt = new int[ptx.length];
+        for(int i = 0 ; i < pt.length ; i ++)
+            pt[i] = ptx[i];
+        return pt;
+    }
+    
+    public int[] getYpts() {
+        int[] pt = new int[pty.length];
+        for ( int i = 0 ; i < pt.length ; i ++ )
+            pt[i] = pty[i];
+        return pt;
+    }
+    
     private void getUpperleft(){
          this.x = 10000000;
          this.y = 10000000;
@@ -89,103 +105,78 @@ public class elPolygon extends ElShape {
     @Override
     public Shape getShape() {
         
-        AffineTransform at = AffineTransform.getRotateInstance(angel, getCenterX(), getCenterY());
+        AffineTransform at = AffineTransform.getRotateInstance(angel, 
+                getCenterX(), getCenterY());
         Shape rotated = at.createTransformedShape(new Polygon(ptx, pty, n));
         return rotated;
     }
+    
+    /**
+     *
+     * @param width
+     */
     @Override
     public void move(int xDisplacment, int yDisplacment){
             //cloneSelf();
-            setX(xDisplacment);
-            setY(yDisplacment);
-            makePoints();
+//            for(int i = 0 ; i < ptx.length ; i ++) {
+//                setX(ptx[i]+xDisplacment);
+//                setY(pty[i]+yDisplacment);
+//            }
      //   makePoints();
+        setX(getClonedX()-xDisplacment);
+        setY(getClonedY()-yDisplacment);
+        makePoints();
     }
     @Override 
     public void setX(int x){
-        //cloneSelf();
-        this.x = this.x - x ;
-        this.CenterX = this.CenterX - x  ;
-       // makePoints();
+        this.x = x;
+        this.CenterX = x ;
+        //makePoints();
     }
     @Override
     public void setY(int y){
-        //cloneSelf();
-        this.y=this.y - y;
-        this.CenterY = this.CenterY - y;
-       // makePoints();
+        this.y = y;
+        this.CenterY = y;
+        //makePoints();
     }
     @Override
     public ElShape getCopy() {
-        return new elPolygon(getCenterX(), getCenterY(), getRadius(), getN(), getAngel());
+        return new elPolygon(getCenterX(), getCenterY(), getRadius(), getN(), 
+                getAngel());
     }
 
-    /**
-     * @return the radius
-     */
     public int getRadius() {
         return radius;
     }
 
-    /**
-     * @param radius the radius to set
-     */
     public void setRadius(int radius) {
         this.radius = radius;
     }
 
-    /**
-     * @return the n
-     */
     public int getN() {
         return n;
     }
 
-    /**
-     * @param n the n to set
-     */
-    public void setN(int n) {
-        this.n = n;
-    }
-
-    /**
-     * @return the angel
-     */
     public double getAngel() {
         return angel;
     }
 
-    /**
-     * @param angel the angel to set
-     */
     public void setAngel(double angel) {
         this.angel = angel;
     }
 
-    /**
-     * @return the CenterX
-     */
     public int getCenterX() {
         return CenterX;
     }
 
-    /**
-     * @param CenterX the CenterX to set
-     */
     public void setCenterX(int CenterX) {
         this.CenterX = CenterX;
     }
 
-    /**
-     * @return the CenterY
-     */
     public int getCenterY() {
         return CenterY;
     }
 
-    /**
-     * @param CenterY the CenterY to set
-     */
     public void setCenterY(int CenterY) {
         this.CenterY = CenterY;
     }
@@ -197,68 +188,6 @@ public class elPolygon extends ElShape {
     @Override        
     public void setHeight(int height) {
         this.height = Math.max(height, 3);
-        
+         makePoints();
     }
 }
-
-//    public Shape move(int displacement , SelectionBox.ResizeBoxType selectedResizeBoxType){
-//        switch(selectedResizeBoxType){
-//            case NW:
-//                for(int i=0;i<n;i++){
-//                    ptx[i] -= displacement;
-//                    pty[i] -= displacement;
-//                }
-//                break;
-//            case N:
-//                for(int i=0;i<n;i++){
-//                  //  ptx[i] += displacement;
-//                    pty[i] -= displacement;
-//                }
-//                break;
-//            case NE:
-//                for(int i=0;i<n;i++){
-//                    ptx[i] += displacement;
-//                    pty[i] -= displacement;
-//                }
-//                break;
-//            case E:
-//                for(int i=0;i<n;i++){
-//                    ptx[i] += displacement;
-//                   // pty[i] += displacement;
-//                }
-//                break;
-//            case W:
-//                for(int i=0;i<n;i++){
-//                    ptx[i] -= displacement;
-//                   // pty[i] += displacement;
-//                }
-//                break;
-//                
-//            case S:
-//                for(int i=0;i<n;i++){
-//                //    ptx[i] -= displacement;
-//                    pty[i] += displacement;
-//                }
-//                break;
-//            case SE:
-//                for(int i=0;i<n;i++){
-//                    ptx[i] += displacement;
-//                    pty[i] += displacement;
-//                }
-//                break;
-//            case SW:
-//                for(int i=0;i<n;i++){
-//                    ptx[i] -= displacement;
-//                    pty[i] += displacement;
-//                }
-//                break;    
-//        }
-//        return getShape();
-//    }
-//    public void Move(int displacement ,){
-//        switch(){
-//            case NE : 
-//                   
-//        }
-        
-//    }
